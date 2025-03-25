493f809f5233d2eadce00af68717ba93d73be5ad
Bourzgui <bourzguifatimazahra@gmail.com>
2025-03-25 15:11:44 +0000

    day 3

--- week-3/day2/challenge/challenge1.js
+++ week-3/day2/challenge/challenge1.js
@@ -3,7 +3,7 @@ for (let i = 1; i <= 6; i++) {
     pattern = "* ".repeat(i).trim(); 
     console.log(pattern);
 }
-
+//m-2
 for (let i = 1; i <= 6; i++) {
     let line = "";
     for (let j = 1; j <= i; j++) {
